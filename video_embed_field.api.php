3a13685501d2d3da693a82bdc0db1066921dfffd
Josh Caldwell <jcaldwell@phase2technology.com>
2011-08-09 18:24:42 -0400

    1244374 - Help Text Not working 1244044 - Teaser Dimensions

--- video_embed_field.api.php
+++ video_embed_field.api.php
@@ -9,7 +9,7 @@
   *  Can be used to add more handlers if needed - from other modules and such
   *  Handler should be an array of the form
   *  array(
-  *    'function' => 'function_name_to_call' //should be of the signature function_name($url) and should return the embed code
+  *    'function' => 'function_name_to_call' //should be of the signature function_name($url, $teaser=FALSE) and should return the embed code
   *    'domain' => 'youtube.com' //the domain that this handler will create embed code for
   *    'form' => 'function_name_for_form' //function to create settings form (optional)
   *    'title' => 'Title' //The title of the handler - to be used as the field group header - will be wrapped with t()
