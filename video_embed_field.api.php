a2485f2670d4b51908507aa65a33e136fadfc462
Josh Caldwell <jcaldwell@phase2technology.com>
2011-08-09 10:37:21 -0400

    Updated comments and added api file

---
+++ video_embed_field.api.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *  API Info for video_embed_field module
+ */
+ 
+ /**
+  *  Creates a hook that other modules can implement to get handlers - 
+  *  hook_video_embed_handler_info
+  *  Can be used to add more handlers if needed - from other modules and such
+  *  Handler should be an array of the form
+  *  array(
+  *    'function' => 'function_name_to_call' //should be of the signature function_name($url) and should return the embed code
+  *    'domain' => 'youtube.com' //the domain that this handler will create embed code for
+  *    'form' => 'function_name_for_form' //function to create settings form (optional)
+  *    'title' => 'Title' //The title of the handler - to be used as the field group header - will be wrapped with t()
+  *  );
+  */
+ function hook_video_embed_handler_info(){
+ 
+ }
