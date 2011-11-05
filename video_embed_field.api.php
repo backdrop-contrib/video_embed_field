39ca7d2b495d3c286a73595576034172c631c9f3
jdelaune <jdelaune@221033.no-reply.drupal.org>
2011-11-05 14:23:56 -0400

    Better Settings Storage - added Video Styles

--- video_embed_field.api.php
+++ video_embed_field.api.php
@@ -1,21 +1,22 @@
 <?php
+
 /**
- *  API Info for video_embed_field module
+ * API Info for video_embed_field module
  */
- 
- /**
-  *  Creates a hook that other modules can implement to get handlers - 
-  *  hook_video_embed_handler_info
-  *  Can be used to add more handlers if needed - from other modules and such
-  *  Handler should be an array of the form
-  *  array(
-  *    'function' => 'function_name_to_call' //should be of the signature function_name($url, $settings, $type = 'video|teaser|image', $image_size='small|medium|large') and should return the embed code
-  *    'defaults' => array() //The default settings for the module, used for both the form and the callback function
-  *    'domain' => 'youtube.com' //the domain that this handler will create embed code for
-  *    'form' => 'function_name_for_form' //function to create settings form (optional)
-  *    'title' => 'Title' //The title of the handler - to be used as the field group header - will be wrapped with t()
-  *  );
-  */
- function hook_video_embed_handler_info(){
- 
- }
+
+/**
+ * Creates a hook that other modules can implement to get handlers - 
+ * hook_video_embed_handler_info
+ * Can be used to add more handlers if needed - from other modules and such
+ * Handler should be an array of the form
+ * array(
+ *   'title' => 'Title', //The title of the handler - to be used as the field group header - will be wrapped with t()
+ *   'function' => 'function_name_to_call', //should be of the signature function_name($url, $settings) and should return the embed code
+ *   'form' => 'function_name_for_form', //function to create settings form (optional)
+ *   'domains' => array('youtube.com'), //the domains that this handler will create embed code for
+ *   'defaults' => array(), //The default settings for the module, used for both the form and the callback function
+ * );
+ */
+function hook_video_embed_handler_info() {
+
+}
